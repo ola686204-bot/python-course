40f90c8d47bc3f2be9b280a0622488d38be2f2f6
Awwal <ola686204@gmail.com>
2026-07-15 11:07:37 +0100

    project one

---
+++ hello.py
@@ -0,0 +1,15 @@
+"""
+Hello.py,
+this program is used to print command.
+@author: Ajadi Awwal Adedeji.
+@date: 14th of July,2026
+"""
+
+
+print("Ajadi Awwal Adedeji")    
+print("17")    
+print("Abuja FCT")  
+print("The author likes studying, sports activities like football. ")  
+print("Artist,Programmer and Footballer")    
+print("Thank you for going through the author's profile")   
+print() # To print  a blank line

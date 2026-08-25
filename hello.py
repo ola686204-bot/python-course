fee6463e8e053a20b4ebbd17fa8231f5116a489e
Awwal <ola686204@gmail.com>
2026-08-25 12:27:05 +0100

    to print command

--- hello.py
+++ hello.py
@@ -6,10 +6,10 @@ this program is used to print command.
 """
 
 
-print("Ajadi Awwal Adedeji")    
-print("17")    
-print("Abuja FCT")  
-print("The author likes studying, sports activities like football. ")  
-print("Artist,Programmer and Footballer")    
-print("Thank you for going through the author's profile")   
-print() # To print  a blank line
+print("Ajadi Awwal Adedeji")
+print("17")
+print("Abuja FCT")
+print("The author likes studying, sports activities like football. ")
+print("Artist,Programmer and Footballer")
+print("Thank you for going through the author's profile")
+print()   # To print  a blank line
